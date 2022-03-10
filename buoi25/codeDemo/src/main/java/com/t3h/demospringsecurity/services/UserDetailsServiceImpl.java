fed6f678e7e6ae50e73c140400d9720b21278544
Nguyễn Đắc Kiên <ndkien98@gmail.com>
2022-03-10 16:30:30 +0700

    demo spring security

---
+++ buoi25/codeDemo/src/main/java/com/t3h/demospringsecurity/services/UserDetailsServiceImpl.java
@@ -0,0 +1,55 @@
+package com.t3h.demospringsecurity.services;
+
+import com.t3h.demospringsecurity.dao.IRoleRepository;
+import com.t3h.demospringsecurity.dao.IUserRepository;
+import com.t3h.demospringsecurity.model.entity.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl  implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository iUserRepository;
+
+    @Autowired
+    private IRoleRepository iRoleRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        AppUser appUser = this.iUserRepository.findByUserName(userName);
+
+        if (appUser == null) {
+            System.out.println("User not found! " + userName);
+            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+        }
+
+        System.out.println("Found User: " + appUser);
+
+        // [ROLE_USER, ROLE_ADMIN,..]
+        List<String> roleNames = this.iRoleRepository.findRoleNamesByUserId(appUser.getUserId());
+
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        if (roleNames != null) {
+            for (String role : roleNames) {
+                // ROLE_USER, ROLE_ADMIN,..
+                GrantedAuthority authority = new SimpleGrantedAuthority(role);
+                grantList.add(authority);
+            }
+        }
+
+        UserDetails userDetails = (UserDetails) new User(appUser.getUserName(), //
+                appUser.getEncrytedPassword(), grantList);
+
+        return userDetails;
+    }
+}
